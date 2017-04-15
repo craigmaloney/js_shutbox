b5362fa1f65fd8992fc7612d90dd79ab9b8c4621
Craig Maloney <craig@decafbad.net>
2017-04-14 22:27:07 -0400

    Adding dice rolls. Starting to look like a game.

--- shutbox.js
+++ shutbox.js
@@ -1,6 +1,13 @@
 var tiles = document.getElementById('tiles');
 var roll_button = document.getElementById('roll');
 var sum_label = document.getElementById('checked_sum');
+var dice1_label = document.getElementById('dice1');
+var dice2_label = document.getElementById('dice2');
+var dice1;
+var dice2;
+var dice_sum;
+
+roll_both_dice();
 
 function tile_sum() {
     var checked_sum = 0;
@@ -21,6 +28,18 @@ function display_sum_total() {
     sum_label.textContent = checked_sum;
 }
 
+function roll_die() {
+    return(Math.floor(Math.random() * 6 + 1));
+}
+
+function roll_both_dice() {
+    dice1 = roll_die();
+    dice2 = roll_die();
+    dice_sum = dice1 + dice2;
+    dice1_label.textContent = dice1;
+    dice2_label.textContent = dice2;
+}
+
 tiles.onclick = function() {
     display_sum_total();
 };
@@ -37,4 +56,5 @@ roll_button.onclick = function() {
 
     }
     display_sum_total();
+    roll_both_dice();
 };
