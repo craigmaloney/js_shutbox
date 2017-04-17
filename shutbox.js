ae98d88864b9e5d6c83cd3ff9d471412fc706415
Craig Maloney <craig@decafbad.net>
2017-04-17 15:43:04 -0400

    Cleaning up the code some more

--- shutbox.js
+++ shutbox.js
@@ -1,8 +1,8 @@
-var tiles = document.getElementById('tiles');
-var roll_button = document.getElementById('roll');
-var sum_label = document.getElementById('checked_sum');
-var dice1_label = document.getElementById('dice1');
-var dice2_label = document.getElementById('dice2');
+const tiles = document.getElementById("tiles");
+const roll_button = document.getElementById("roll");
+const sum_label = document.getElementById("checked_sum");
+const dice1_label = document.getElementById("dice1");
+const dice2_label = document.getElementById("dice2");
 var dice1;
 var dice2;
 var dice_sum;
@@ -12,11 +12,12 @@ roll_both_dice();
 
 function tile_sum() {
     var checked_sum = 0;
-    var tile_input = tiles.getElementsByTagName('input');
-    for (var i = 0, length = tile_input.length; i < length; i++) {
-        if (tile_input[i].type == 'checkbox') {
+    var tile_input = tiles.getElementsByTagName("input");
+    var i;
+    for (i = 0; i < tile_input.length; i++) {
+        if (tile_input[i].type === "checkbox") {
            if (tile_input[i].checked && tile_input[i].disabled === false) {
-                checked_sum += parseInt(tile_input[i].value);
+                checked_sum += parseInt(tile_input[i].value, 10);
            }
         }
 
@@ -45,14 +46,45 @@ tiles.onclick = function() {
     display_sum_total();
 };
 
+function check_end(sum_tiles) {
+    var orig_sum = sum_tiles;
+    var i;
+    var tile_input;
+    var result;
+
+    if (sum_tiles === 0) {
+        return true;
+    }
+
+    if (sum_tiles > 9) {
+       sum_tiles = 9;
+    } 
+
+    for (i=sum_tiles; i >= 1; i--) {
+        tile_input = tiles.getElementsByTagName("input")[i - 1];
+        if (tile_input.disabled === false) {
+            if ((orig_sum - i) >= i) {
+                return false;
+            }
+
+            result = check_end(orig_sum - i);
+            if (result) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
 roll_button.onclick = function() {
     var checked_sum = tile_sum();
-    if (dice_sum == checked_sum) {
+    var i;
+    if (dice_sum === checked_sum) {
 
-        var tile_input = tiles.getElementsByTagName('input');
-        for (var i = 0, length = tile_input.length; i < length; i++) {
-            if (tile_input[i].type == 'checkbox') {
+        var tile_input = tiles.getElementsByTagName("input");
+        for (i = 0; i < tile_input.length; i++) {
+            if (tile_input[i].type === "checkbox") {
                 if (tile_input[i].checked) {
                         tile_input[i].disabled = true;
                 }
@@ -62,34 +94,7 @@ roll_button.onclick = function() {
         display_sum_total();
         roll_both_dice();
         if (check_end(dice_sum) === false) {
-            alert('No more valid moves');
+            alert("No more valid moves");
         }
     }
 };
-
-function check_end(sum_tiles) {
-    var orig_sum = sum_tiles;
-    if (sum_tiles === 0) {
-        return true;
-    }
-
-    if (sum_tiles > 9) {
-       sum_tiles = 9;
-    } 
-
-    for (var i=sum_tiles; i >= 1; i--) {
-        var tile_input = tiles.getElementsByTagName('input')[i - 1];
-        if (tile_input.disabled === false) {
-            if ((orig_sum - i) >= i) {
-                return false;
-            }
-
-            var result = check_end(orig_sum - i);
-            if (result) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
